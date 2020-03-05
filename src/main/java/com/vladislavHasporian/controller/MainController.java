de328295cc7be724f78cbfa81b4f59cb24a933ac
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-03-05 09:15:46 +0200

    add @InitBinder - removes whitespace

--- src/main/java/com/vladislavHasporian/controller/MainController.java
+++ src/main/java/com/vladislavHasporian/controller/MainController.java
@@ -6,10 +6,13 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,12 @@ public class MainController {
 	@Autowired
 	public MainController(IStudentService studentService) {
 		this.studentService = studentService;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimerEditor);
 	}
 	
 	@GetMapping
